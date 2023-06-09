303adefc2c16c43559f19e480863f8458acb1cfb
VaishaliThakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2023-06-10 09:50:00 +1000

    Update your_app.py

--- your_app.py
+++ your_app.py
@@ -1,31 +1,53 @@
+import gdown
+from tensorflow.keras.models import load_model
 import streamlit as st
+from PIL import Image
+import numpy as np
 
-# Define the recyclable items
-recyclables = {
-    'Paper': ['Newspapers', 'Magazines', 'Cardboard'],
-    'Plastic': ['Bottles', 'Containers', 'Bags'],
-    'Glass': ['Bottles', 'Jars'],
-    'Metal': ['Cans', 'Aluminum foil'],
-    'Electronics': ['Computers', 'Phones', 'Printers']
-}
+# Define the URL of the model file in the cloud storage
+model_url = 'https://drive.google.com/drive/folders/1lPhuBWhJ5nF8118nRPV3X-vADUkelAp5?usp=sharing'
 
-# Create the Streamlit app
+# Download the model file from the cloud storage
+model_path = 'waste_classification_model.v4'
+gdown.download(model_url, model_path, quiet=False)
+
+# Load the model
+model = load_model(model_path)
+
+# Load the trained model for waste classification
+model = load_model('waste_classification_model.h5')
+
+# Define the recycling categories
+recycling_categories = ['Glass', 'Paper', 'Plastic', 'Metal', 'Organic', 'Others']
+
+# Create the Streamlit web app
 def main():
-    st.title('Recycling Guide')
+    st.title("Recycling App")
 
-    # Display instructions
-    st.write('Select the type of item you want to recycle:')
+    # Add an image uploader for the user to upload waste images
+    uploaded_image = st.file_uploader("Upload an image of the waste")
 
-    # Create a dropdown to select recyclable item
-    recyclable_type = st.selectbox('Recyclable Type', list(recyclables.keys()))
+    if uploaded_image is not None:
+        # Load and preprocess the uploaded image
+        image = Image.open(uploaded_image)
+        processed_image = preprocess_image(image)
 
-    # Display the recyclable items based on the selected type
-    if recyclable_type:
-        st.write('You can recycle the following items:')
-        for item in recyclables[recyclable_type]:
-            st.write(f'- {item}')
+        # Make predictions using the trained model
+        predictions = model.predict(processed_image)
+        predicted_class = recycling_categories[np.argmax(predictions)]
 
-# Run the app
-if __name__ == '__main__':
+        # Display the predicted recycling category
+        st.subheader("Predicted Recycling Category:")
+        st.write(predicted_class)
+
+# Preprocess the image (resize, normalize, etc.) for model input
+def preprocess_image(image):
+    # Apply any necessary preprocessing steps (resize, normalize, etc.)
+    processed_image = ...
+
+    # Return the processed image
+    return processed_image
+
+if __name__ == "__main__":
     main()
 
