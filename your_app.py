9c450a77948945c0960a4602d3c46bc007bf0ad3
VaishaliThakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2023-06-10 11:13:42 +1000

    Update your_app.py

--- your_app.py
+++ your_app.py
@@ -1,33 +1,10 @@
-# Load the pre-trained model
-model = tf.keras.applications.MobileNetV2()
+import streamlit as st
 
-# Define the labels for ImageNet classes
-LABELS_URL = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
-labels = tf.keras.utils.get_file("ImageNetLabels.json", LABELS_URL)
+st.title("Simple Streamlit App")
 
-# Load the ImageNet labels
-with open(labels) as f:
-    imagenet_labels = np.array(f.read().splitlines())
+# Text input
+user_input = st.text_input("Enter some text")
 
-# Streamlit app code
-st.title("Image Classification with TensorFlow")
+# Echo the entered text
+st.write("You entered:", user_input)
 
-# Upload and classify an image
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    # Display the uploaded image
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
-    # Preprocess the image
-    resized_image = image.resize((224, 224))
-    normalized_image = np.array(resized_image) / 255.0
-    input_image = np.expand_dims(normalized_image, axis=0)
-
-    # Classify the image
-    predictions = model.predict(input_image)
-    predicted_label = imagenet_labels[np.argmax(predictions)]
-
-    # Display the predicted label
-    st.subheader("Prediction:")
-    st.write(predicted_label)
