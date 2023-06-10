5c44bbad8df24fe1c8370906c141157342e41d81
VaishaliThakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2023-06-10 11:18:02 +1000

    Update your_app.py

--- your_app.py
+++ your_app.py
@@ -1,16 +1,41 @@
 import streamlit as st
-import pandas as pd
+import tensorflow as tf
+import numpy as np
+import requests
+from PIL import Image
 
-st.title("CSV File Viewer")
+# Load pre-trained model
+model = tf.keras.applications.MobileNetV2()
 
-# Upload a CSV file
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+# Define labels
+labels_path = tf.keras.utils.get_file("ImageNetLabels.txt", 
+                                      "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt")
+with open(labels_path) as f:
+    labels = f.readlines()
+labels = [label.strip() for label in labels]
+
+st.title("Image Classifier")
+
+# Upload an image
+uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(uploaded_file)
+    # Read the image
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Uploaded Image", use_column_width=True)
 
-    # Display the DataFrame
-    st.dataframe(df)
+    # Preprocess the image
+    img = img.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Make predictions
+    predictions = model.predict(img_array)
+    predicted_label = np.argmax(predictions)
+    predicted_class = labels[predicted_label]
+
+    # Display the predicted class
+    st.write(f"Predicted Class: {predicted_class}")
+
 
 
