5ebff226bcb6f7ea476f0c703657bf0f0a8fc276
mthakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2023-06-10 14:01:04 +1000

    changes

--- your_app.py
+++ your_app.py
@@ -8,6 +8,16 @@ model = tf.keras.models.load_model('waste_classification_model.h5')
 # Define the recycling categories
 recycling_categories = ['Cardboard', 'Glass', 'Metal', 'Paper', 'Plastic', 'Trash']
 
+# Define recycling instructions for each category
+recycling_instructions = {
+    'Cardboard': 'Please flatten the cardboard and place it in the recycling bin.',
+    'Glass': 'Please separate the glass by color (clear, green, or brown) and place it in the recycling bin.',
+    'Metal': 'Please rinse the metal item, remove any non-metal parts, and place it in the recycling bin.',
+    'Paper': 'Please remove any plastic or metal components, such as staples or clips, and place the paper in the recycling bin.',
+    'Plastic': 'Please check the recycling symbol on the plastic item to determine if it is recyclable in your area. If recyclable, rinse the item and place it in the recycling bin.',
+    'Trash': 'The item is not recyclable. Please dispose of it in the regular trash bin.'
+}
+
 # Create the Streamlit web app
 def main():
     st.title("Recycling App")
@@ -24,10 +34,15 @@ def main():
         # Make predictions using the trained model
         predictions = model.predict(processed_image)
         predicted_class = recycling_categories[np.argmax(predictions)]
+        recycling_instruction = recycling_instructions.get(predicted_class, "No recycling information available.")
 
         # Display the predicted recycling category
         st.subheader("Predicted Recycling Category:")
         st.write(predicted_class)
+
+        # Display recycling instructions
+        st.subheader("Recycling Instructions:")
+        st.write(recycling_instruction)
 
 # Preprocess the image (resize, normalize, etc.) for model input
 def preprocess_image(image):
